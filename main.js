fa4d19497f2791a3f3597bc2fe37898529f877fc
Austin Marler <austinnmarler@gmail.com>
2019-03-27 14:11:26 -0700

    completed explore mode

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 
+    $('#normalCalculatorForm').on('click', 'button', function(){
+
+        var num1 = Number($('#num1').val());
+        var num2 = Number($('#num2').val());
+
+        if ($(this).hasClass('add')) {
+            $('#answer').html(num1 + num2);
+        } else if ($(this).hasClass('subtract')) {
+            $('#answer').html(num1 - num2);
+        } else if ($(this).hasClass('multiply')) {
+            $('#answer').html(num1 * num2);
+        } else if ($(this).hasClass('divide')) {
+            $('#answer').html(num1 / num2);
+        }
+        
+    })
+
 })
